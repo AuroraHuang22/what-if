ad6e20c45fc59d6a96435d474b78fcbc2a1a30ae
AuroraHuang22 <aurora90376@gmail.com>
2025-01-05 23:50:27 +0800

    🔧 Add configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   css: ['@/assets/css/tailwind.css'],
   modules: ['@nuxt/ui'],
-})
+  app: {
+    baseURL: process.env.NODE_ENV === 'production' ? '/what-if/' : '/',
+  },
+  generate: {
+    routes: ['/', '/404'],
+    exclude: [/\/dynamic\/.*/]
+  }
+});
---
+++ pages/404.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Page Not Found</h1>
+    <p>We couldn't find the page you were looking for.</p>
+  </div>
+</template>
