2718ae9e395addf3de9eb45e586a8788e063f924
AuroraHuang22 <aurora90376@gmail.com>
2025-01-06 01:44:17 +0800

    🐛 Update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,22 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['@/assets/css/tailwind.css'],
   modules: ['@nuxt/ui'],
 
+  app: {
+    baseURL: '/what-if/', // 設置 baseURL 為子目錄路徑
+  },
+
   generate: {
-    routes: ['/', '/404'],
+    routes: ['/', '/404'], // 指定要生成的靜態路由
   },
 
   nitro: {
     prerender: {
       crawlLinks: false,
-      routes: [
-        '/', '/404',
-      ],
+      routes: ['/', '/404'], // 明確指定要預渲染的路由
     },
   },
 
-  compatibilityDate: '2025-01-06'
+  compatibilityDate: '2025-01-06',
 });
